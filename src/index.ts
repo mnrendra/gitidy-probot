fccb8ee6c633453928f812264c272c865084e3ed
Muhammad Rendra <muhammadnurrendra@gmail.com>
2023-01-02 17:56:55 +0700

    Sanitize `src/index.ts` & `test/index.test.ts` • Sanitize `src/index.ts` & `test/index.test.ts` based on `standard.js` • Add `console.log` on `issues.opened` webhook

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
-import { Probot } from "probot";
+import { Probot } from 'probot'
 
 export = (app: Probot) => {
-  app.on("issues.opened", async (context) => {
+  app.on('issues.opened', async (context) => {
     const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    await context.octokit.issues.createComment(issueComment);
-  });
+      body: 'Thanks for opening this issue!',
+    })
+
+    console.log('issueComment:', issueComment)
+
+    await context.octokit.issues.createComment(issueComment)
+  })
+
   // For more information on building apps:
   // https://probot.github.io/docs/
 
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
-};
+}
